5db756e7aed91f7a92182585bde6bec72ee6026f
TinaXEXE <ryabenkoka@college.omsu.ru>
2025-04-19 09:17:40 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+/*
+Welcome to JDoodle!
+
+You can execute code here in 88 languages. Right now you’re in the Java IDE.
+
+  1. Click the orange Execute button ▶ to execute the sample code below and see how it works.
+
+  2. Want help writing or debugging code? Type a query into JDroid on the right hand side ---------------->
+
+  3.Try the menu buttons on the left. Save your file, share code with friends and open saved projects.
+
+Want to change languages? Try the search bar up the top.
+*/
+
+import java.util.Scanner;
+
+public class Main {
+
+   public static void main(String[] args) {
+
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Введите число:");
+
+       int number = sc.nextInt();
+
+       System.out.println("Спасибо! Вы ввели число " + number);
+
+       sc.close();
+
+   }
+}
